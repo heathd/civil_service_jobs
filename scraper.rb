7e6600434e8b9f66191795be27865e768baf4be1
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-06-30 18:28:15 +0100

    test

--- scraper.rb
+++ scraper.rb
@@ -1,20 +1,18 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'pry'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
 # agent = Mechanize.new
-#
+
 # # Read in a page
-# page = agent.get("http://foo.com")
-#
+# page = agent.get("https://www.civilservicejobs.service.gov.uk/csr/index.cgi")
+
 # # Find something on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
+# f = page.form('#ID_context_search_form')
+
+# Write out to the sqlite database using scraperwiki library
+ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
 
