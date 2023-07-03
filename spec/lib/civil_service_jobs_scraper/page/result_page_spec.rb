4e9c7e630c6d517d7e13f805f3e122333d33fc09
David Heath <david.heath@digital.cabinet-office.gov.uk>
2023-07-03 01:07:46 +0100

    fix

--- spec/lib/civil_service_jobs_scraper/page/result_page_spec.rb
+++ spec/lib/civil_service_jobs_scraper/page/result_page_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CivilServiceJobsScraper::Page::ResultPage do
 	let(:fixture_file_uri) { "file://" + (spec_path + "result_page1.html").to_s }
 	let(:page) { agent.get(fixture_file_uri) }
 	let(:status_display) { instance_double(CivilServiceJobsScraper::StatusDisplay) }
-	subject(:result_page) { CivilServiceJobsScraper::Page::ResultPage.new(page, status_display: status_display) }
+	subject(:result_page) { CivilServiceJobsScraper::Page::ResultPage.new(page) }
 	
 	it "identifies the last page by scraping the pagination indicator" do
 		expect(result_page.last_page).to eq(95)
