dd95299670e1bd65ac5cb311ac2aa0f63bcc3622
David Heath <david.heath@digital.cabinet-office.gov.uk>
2024-08-24 20:27:52 +0100

    Allow AWS access credentials provided in the environment to override local dev ones

--- config/aws_record_local_config.rb
+++ config/aws_record_local_config.rb
@@ -1,11 +1,15 @@
 require 'aws-record'
 require 'aws-sdk-dynamodb'
 
-dynamo_db_client = Aws::DynamoDB::Client.new(
-  region: "localhost",
-  endpoint: "http://localhost:8000",
-  credentials: Aws::Credentials.new('foo', 'bar')
-)
+dynamo_db_client = if ENV.has_key?("AWS_ACCESS_KEY_ID")
+  Aws::DynamoDB::Client.new
+else
+  Aws::DynamoDB::Client.new(
+    region: "localhost",
+    endpoint: "http://localhost:8000",
+    credentials: Aws::Credentials.new('foo', 'bar')
+  )
+end
 
 CivilServiceJobsScraper::DynamoDbResultStore.configure_client(dynamo_db_client)
 CivilServiceJobsScraper::DynamoDbResultStore.ensure_table_exists!
