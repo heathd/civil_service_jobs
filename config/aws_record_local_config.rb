c0f321d1936970663f29e235f95b5f72b4f88d8d
David Heath <david.heath@digital.cabinet-office.gov.uk>
2024-08-24 15:51:14 +0100

    Correctly configure connection to local dynamodb
    for use during local testing

--- config/aws_record_local_config.rb
+++ config/aws_record_local_config.rb
@@ -1,7 +1,11 @@
-CivilServiceJobsScraper::DynamoDbResultStore::JobRecord.configure_client(
+require 'aws-record'
+require 'aws-sdk-dynamodb'
+
+dynamo_db_client = Aws::DynamoDB::Client.new(
   region: "localhost",
-  access_key_id: "foo",
-  secret_access_key: "bar",
-  endpoint: "http://localhost:8000"
+  endpoint: "http://localhost:8000",
+  credentials: Aws::Credentials.new('foo', 'bar')
 )
+
+CivilServiceJobsScraper::DynamoDbResultStore.configure_client(dynamo_db_client)
 CivilServiceJobsScraper::DynamoDbResultStore.ensure_table_exists!
